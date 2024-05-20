bdc2c9c58cbc0e99b1347aeb2a24b11b10e6a5ef
eliasaurio <elias.arancibia.m@gmail.com>
2024-05-19 20:18:41 -0400

    Modificaciones room, usar objetos

--- JuegoDungeonexplorer/src/contenidoSalas/Pocion.java
+++ JuegoDungeonexplorer/src/contenidoSalas/Pocion.java
@@ -4,16 +4,20 @@ public class Pocion extends GameObject {
 
 
     public Pocion(String nombreObjeto, String descripcionObjeto) {
+
         super(nombreObjeto, descripcionObjeto);
     }
-
+//Funcion de usar poción, personaje se hizo para que funcione
     @Override
     public void usar() {
-        super.usar();
+        Personaje jug1 = new Personaje();
+        jug1.heal(1);
+        System.out.println("Has tomado la poción, has recuperado 1 vida");
     }
 
     @Override
     public String getDescripcionObjeto() {
+
         return super.getDescripcionObjeto();
     }
 }
