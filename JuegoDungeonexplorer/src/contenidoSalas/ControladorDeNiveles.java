8f3f888a920c102937e6e337ecbdc714c7ae8b6e
Xhairo <xhairo94@gmail.com>
2024-05-19 20:40:48 -0400

    controlador casi pulento

--- JuegoDungeonexplorer/src/contenidoSalas/ControladorDeNiveles.java
+++ JuegoDungeonexplorer/src/contenidoSalas/ControladorDeNiveles.java
@@ -1,39 +1,37 @@
+package contenidoSalas;
+
 import java.util.*;
 
 public class ControladorDeNiveles {
 
     private boolean keyisUsed=false;
     public String index ="0.0";
-    private final Map<String, String> niveles = new HashMap<>();
-    String idRoomActual;
+    private final Map<String, room> niveles = new HashMap<>();
+    private room roomActual;
     Scanner sc = new Scanner(System.in);
   
     //Iniciar el juego y niveles
     public void initGame(){
-        niveles.put("0.0", "rewardRoom");
-        niveles.put("1.0", "emptyBlockRoom");
-        niveles.put("2.0", "enemyRoom");
-        niveles.put("3.0", "emptyRoom");
-        niveles.put("2.1", "rewardRoom");
-        niveles.put("2.2", "enemyRoom");
-        niveles.put("3.1", "rewardRoom");
-        niveles.put("3.2", "enemyRoom");
-        niveles.put("4.0", "emptyRoom");
-        niveles.put("5.0", "enemyRoom");
-        niveles.put("6.0", "emptyRoom");
-        niveles.put("6.1", "enemyRoom");
-        niveles.put("6.2", "emptyRoom");
+        niveles.put("0.0", new salaTesoro());
+        niveles.put("1.0", new salaVacia());
+        niveles.put("2.0", new salaEnemigo());
+        niveles.put("3.0", new salaVacia());
+        niveles.put("2.1", new salaTesoro());
+        niveles.put("2.2", new salaEnemigo());
+        niveles.put("3.1", new salaTesoro());
+        niveles.put("3.2", new salaEnemigo());
+        niveles.put("4.0", new salaVacia());
+        niveles.put("5.0", new salaEnemigo());
+        niveles.put("6.0", new salaVacia());
+        niveles.put("6.1", new salaEnemigo());
+        niveles.put("6.2", new salaVacia());
 
-        idRoomActual = niveles.get("0.0");
+        roomActual = new salaTesoro();
 
     }
 
     public ControladorDeNiveles() {
         initGame();
-    }
-
-    public ControladorDeNiveles(boolean keyisUsed) {
-        this.keyisUsed = keyisUsed;
     }
 
     public boolean isKeyisUsed() {
@@ -43,26 +41,8 @@ public class ControladorDeNiveles {
     public void setKeyisUsed(boolean keyisUsed) {
         this.keyisUsed = keyisUsed;
     }
-
-    public String getIndex() {
-        return index;
-    }
-
-    public void setIndex(String index) {
-        this.index = index;
-    }
-
-    public String getIdroomactual() {
-        return idRoomActual;
-    }
-
-    public void setIdroomactual(String idroomactual) {
-        this.idRoomActual = idroomactual;
-    }
-
-    //Leer el Nivel
-    public void readlevel(){
-        System.out.println(idRoomActual);
+    public room getroomActual() {
+        return roomActual;
     }
     //SubMenu
     public int opcion;
@@ -71,7 +51,8 @@ public class ControladorDeNiveles {
         switch (index) {
                 case "0.0": //room1.0
                     if(keyisUsed){
-                        idRoomActual = niveles.get("1.0");
+                        roomActual = niveles.get("1.0");
+                        index="1.0";
                         keyisUsed=false;
                     }else{
                         System.out.println("La Puerta esta con llave no se puede abrir");
@@ -84,9 +65,11 @@ public class ControladorDeNiveles {
                         sc.nextLine();
                     }while (opcion<0 || opcion>2);
                 if (opcion == 1) {//Room2.0
-                    idRoomActual = niveles.get("2.0");
+                    roomActual = niveles.get("2.0");
+                    index="2.0";
                 } else if (opcion == 2) {//room3.0
-                    idRoomActual = niveles.get("3.0");
+                    roomActual = niveles.get("3.0");
+                    index="3.0";
                 }
                 break;
                 case "2.0":
@@ -96,13 +79,16 @@ public class ControladorDeNiveles {
                         sc.nextLine();
                     }while (opcion<0 || opcion>2);
                 if (opcion == 1) {//room 2.1
-                    idRoomActual = niveles.get("2.1");
+                    roomActual = niveles.get("2.1");
+                    index="2.1";
                 } else if (opcion == 2) {//room 2.2
-                    idRoomActual = niveles.get("2.2");
+                    roomActual = niveles.get("2.2");
+                    index="2.2";
                 }
                 break;
-                case "2.1", "2.2","3.1","3.2":
-                        idRoomActual = niveles.get("4.0");
+                case "2.1", "2.2","3.1","3.2"://room 4.0
+                    roomActual = niveles.get("4.0");
+                        index = "4.0";
                     break;
                 case "3.0":
                     do {
@@ -111,17 +97,20 @@ public class ControladorDeNiveles {
                         sc.nextLine();
                     }while (opcion<0 || opcion>2);
                     if (opcion == 1) {//room 3.1
-                    idRoomActual = niveles.get("3.1");
+                        roomActual = niveles.get("3.1");
+                    index="3.1";
                      } else if (opcion == 2) {//room 3.2
-                    idRoomActual = niveles.get("3.2");
+                        roomActual = niveles.get("3.2");
+                    index="3.2";
                     }
                 break;
-            case "4.0":
-                idRoomActual = niveles.get("5.0");
+            case "4.0"://room 5.0
+                roomActual = niveles.get("5.0");
+                index="5.0";
                 break;
-                case "5.0":
-
-                    idRoomActual = niveles.get("6.0");
+                case "5.0"://room 6.0
+                    roomActual = niveles.get("6.0");
+                    index="6.0";
                     break;
                     case "6.0":
                         do {
@@ -129,21 +118,22 @@ public class ControladorDeNiveles {
                             opcion= sc.nextInt();
                             sc.nextLine();
                         }while (opcion<0 || opcion>2);
-                        if (opcion == 1) {//room 3.1
-                            idRoomActual = niveles.get("6.1");
-                        } else if (opcion == 2) {//room 3.2
-                            idRoomActual = niveles.get("6.2");
+                        if (opcion == 1) {//room 6.1
+                            roomActual = niveles.get("6.1");
+                            index="6.1";
+                        } else if (opcion == 2) {//room 6.2
+                            roomActual = niveles.get("6.2");
+                            index="6.2";
                         }
                         break;
                         case "6.1":
                             System.out.println("no hay salida");
+
                             break;
             case "6.2":
                 System.out.println("Despertaste del sueño");
                 break;
         }
-        index = getIndex(idRoomActual);
-        readlevel();
     }
     //Retroceder
     public void backtroom() {
@@ -152,16 +142,20 @@ public class ControladorDeNiveles {
                 System.out.println("No puedes retroceder, la ventana no se ve que se pueda abrir, solo queda la pasar por la puerta");
                 break;
             case "1.0":
-                idRoomActual = niveles.get("0.0");
+                roomActual = niveles.get("0.0");
+                index="0.0";
                 break;
             case "2.1", "2.2":
-                idRoomActual = niveles.get("2.0");
+                roomActual = niveles.get("2.0");
+                index="2.0";
                 break;
             case "3.0","2.0":
-                idRoomActual = niveles.get("1.0");
+                roomActual = niveles.get("1.0");
+                index="1.0";
                 break;
             case "3.1","3.2":
-                idRoomActual = niveles.get("3.0");
+                roomActual = niveles.get("3.0");
+                index="3.0";
                 break;
             case "4.0":
                 do {
@@ -170,39 +164,36 @@ public class ControladorDeNiveles {
                     sc.nextLine();
                 }while (opcion<0 || opcion>4);
                 if (opcion==1) {
-                    idRoomActual = niveles.get("2.1");
+                    roomActual = niveles.get("2.1");
+                    index="2.1";
 
                 } else if (opcion==2) {
-                    idRoomActual = niveles.get("2.2");
+                    roomActual = niveles.get("2.2");
+                    index="2.2";
 
                 } else if (opcion==3) {
-                    idRoomActual = niveles.get("3.1");
+                    roomActual = niveles.get("3.1");
+                    index="3.1";
 
                 } else if (opcion==4) {
-                    idRoomActual = niveles.get("3.2");
+                    roomActual = niveles.get("3.2");
+                    index="3.2";
                 }
                 break;
             case "5.0":
-                idRoomActual = niveles.get("4.0");
+                roomActual = niveles.get("4.0");
+                index="4.0";
                 break;
             case "6.0":
-                idRoomActual = niveles.get("5.0");
+                roomActual = niveles.get("5.0");
+                index="5.0";
                 break;
             case "6.1", "6.2":
-                idRoomActual = niveles.get("6.0");
+                roomActual = niveles.get("6.0");
+                index="6.0";
                 break;
         }
-        index = getIndex(idRoomActual);
-        readlevel();
     }
 
-    public String getIndex(String room) {
-        for (Map.Entry<String, String> entry : niveles.entrySet()) {
-            if (entry.getValue().equals(room)) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
 }
 
